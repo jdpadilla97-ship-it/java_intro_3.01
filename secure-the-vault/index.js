bca7f9d0e87a779304f8fe2d1c93c24bc7749fee
Joanna Padilla <jdpadilla97@gmail.com>
2026-05-29 21:09:16 -0700

    Add secure-the-vault project files

---
+++ secure-the-vault/index.js
@@ -0,0 +1,19 @@
+let code1 = 7 + 3
+console.log(code1)
+
+let code2 = 100 / 4 + 15
+console.log(code2)
+
+let code3 = 100 % 3 + 38
+console.log(code3)
+
+message = "The vault has been secured. The combination is "
+console.log(message)
+
+let codeA = code1 + "-" + code2 + "-" + code3
+console.log(codeA)
+
+let codeB = `${code1}-${code2}-${code3}`
+console.log(codeB)
+
+console.log(message, codeA, codeB);
